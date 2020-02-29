e011111391012b22c80dad443385f9f55ca931a9
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-02-29 15:25:49 -0500

    Implement spec for Dessert's #serve method

--- spec/dessert_spec.rb
+++ spec/dessert_spec.rb
@@ -59,7 +59,10 @@ describe Dessert do
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize).and_return("Chef Tucker the Great Baker")
+      expect(cookie.serve).to eq("Chef Tucker the Great Baker has made 10 cookies!")
+    end
   end
 
   describe "#make_more" do
